484ba032b203cd88099c699a4df99a99dc07a2d0
Jay <gift_ij2002@yahoo.com>
2015-10-07 16:01:13 +0100

    rename variable for problem 9

--- problem_9.rb
+++ problem_9.rb
@@ -1,20 +1,20 @@
 def pythagoras(sum)
   return false if sum < 12
 
-  results = []
+  arr = []
   (1..sum/2).each do |z|
     (1..z).each do |y|
       (1..y).each do |x|
-        results << [x, y, z].inject(:*) if (x + y + z == sum) && (x**2 + y**2 == z**2)
+        arr << [x, y, z].inject(:*) if (x + y + z == sum) && (x**2 + y**2 == z**2)
       end
     end
   end
-  to_return = results.join.to_i
+  result = arr.join.to_i
 
-  if to_return == 0
+  if result == 0
     false
   else
-    to_return
+    result
   end
 
 end
